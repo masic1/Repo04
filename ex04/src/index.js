24b7e5b37c5aaf9ae2aa79bb8b2217a0861aa295
masic1 <masic.mirza@gmail.com>
2021-09-16 23:15:15 +0200

    Added the rest ex13-15, modified ex04 index.js

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,10 +1,10 @@
 function main() {
     // only change code below this line
         var myDecimal = 25.4;
-        var MyDecimalE = myDecimal.toExponential(3);
+        var MyDecimalE = myDecimal * 10e-5;
     // only change code above this line
     return {
-        myDecimal,
+        myDecimal,ty
         MyDecimalE
     };
 }
